c14cae27c80d9dbb4b466801c688b61bc22a96ac
Oscar Del Ben <oscar@iThere.local>
2010-01-09 12:38:58 +0100

    spinner

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,4 +3,12 @@ function initialize() {
 	$('regexp').focus();
 }
 
+function formLoading() {
+  $('match_data').innerHtml = '';
+  $('group_matches').innerHtml = '';
+  $('sub').innerHtml = '';
+  $('gsub').innerHtml = '';
+  $('loading').show();
+}
+
 Event.observe( window, 'load' , initialize );
